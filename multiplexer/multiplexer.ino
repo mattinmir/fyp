577d822a3504048f36c0a103ceb6fe26533c8a50
Your Name <you@example.com>
2016-12-14 13:42:25 +0000

    detects voltages

--- multiplexer/multiplexer.ino
+++ multiplexer/multiplexer.ino
@@ -1,5 +1,5 @@
-unsigned intermediary_sel[] = { 25, 26, 27 }; // select pins for intermediary multiplexers
-unsigned output_sel[] = { 22, 23, 24 };
+unsigned intermediary_sel[] = { 25, 26, 27 }; // Select pins for intermediary multiplexers
+unsigned output_sel[] = { 22, 23, 24 }; // Select pins to choose ouput of which multiplexer
 unsigned output = 0;   // Analogue pin to read voltage reading from
 unsigned input_voltage = 5.0;
 
@@ -11,6 +11,8 @@ void setup(){
     pinMode(intermediary_sel[pin], OUTPUT);
     pinMode(output_sel[pin], OUTPUT);
 
+    digitalWrite(output_sel[2], LOW);
+
   } 
 
 }
@@ -18,17 +20,27 @@ void setup(){
 
 void loop()
 {
-   for (unsigned short select = 0; select < 8; select++)
+    // For each muliplexer
+   for (unsigned short i = 0; i < 3; i++)
    {
-      digitalWrite(sel[0], select & 0x1);
-      digitalWrite(sel[1], select & 0x2);
-      digitalWrite(sel[2], select & 0x4);
+      digitalWrite(output_sel[0], i & 0x1);
+      digitalWrite(output_sel[1], i & 0x2);
+      
+      // For each input to the multiplexer
+      for (unsigned short j = 0; j < 8; j++)
+      { 
+        digitalWrite(intermediary_sel[0], j & 0x1);
+        digitalWrite(intermediary_sel[1], j & 0x2);
+        digitalWrite(intermediary_sel[2], j & 0x4);
 
-    
+        Serial.print(analogRead(output) * (input_voltage / 1023.0));
+        Serial.print ("\t");
+
+        
+      }
 
 
-      Serial.print(analogRead(common) * (input_voltage / 1023.0));
-      Serial.print ("\t");
+      
    }
 
    
