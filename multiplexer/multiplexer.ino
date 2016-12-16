1df37174c5df545eca94a4b8f6cb751b0a3a0285
Your Name <you@example.com>
2016-12-16 17:33:29 +0000

    two lines

--- multiplexer/multiplexer.ino
+++ multiplexer/multiplexer.ino
@@ -1,8 +1,8 @@
-unsigned intermediary_sel[] = { 25, 26, 27 }; // Select pins for intermediary multiplexers
-unsigned output_sel[] = { 22, 23, 24 }; // Select pins to choose ouput of which multiplexer
+unsigned intermediary_sel[] = { /*lsb*/ 25, 26, 27 /*msb*/ }; // Select pins for intermediary multiplexers
+unsigned output_sel[] = { /*lsb*/ 22, 23, 24 /*msb*/ }; // Select pins to choose ouput of which multiplexer
 unsigned output = 0;   // Analogue pin to read voltage reading from
 unsigned input_voltage = 5.0;
-float voltages[20]; // Values at each node
+float voltages[10][18]; // Values at each node
 
 void setup(){
   Serial.begin(9600);  // serial comms for troubleshooting (always)
@@ -12,12 +12,8 @@ void setup(){
     pinMode(output_sel[pin], OUTPUT);
   } 
 
-  for (unsigned i = 0; i < 20; i++)
-  {
-    voltages[i]=0.0;
-  }
+
   
-  digitalWrite(output_sel[2], LOW);
 
 
 }
@@ -25,37 +21,46 @@ void setup(){
 
 void loop()
 {
-    // For each muliplexer
-   for (unsigned short i = 0; i < 6; i++)
-   {
-      digitalWrite(output_sel[0], i & 0x1);
-      digitalWrite(output_sel[1], i & 0x2);
-      digitalWrite(output_sel[1], i & 0x4);
-      
-      
-      // For each input to the multiplexer
-      for (unsigned short j = 0; j < 8; j++)
-      { 
-        digitalWrite(intermediary_sel[0], j & 0x1);
-        digitalWrite(intermediary_sel[1], j & 0x2);
-        digitalWrite(intermediary_sel[2], j & 0x4);
 
-        if ((i*8)+j <= 18)
-        {
-          Serial.print(analogRead(output) * (input_voltage / 1023.0));
-          Serial.print ("\t");
-        }
-/*
-        float voltage = analogRead(output) * (input_voltage / 1023.0);
-        
-        voltages[(i*8)+j] = voltage;
-        Serial.println(voltages[(i*8)+j]);
-*/
-        
+unsigned short index;
+
+  // For each line
+  for (unsigned line = 0; line < 2; line++)
+  {
+    Serial.print("Line ");
+    Serial.print(line);
+    Serial.print(":\t");
+    
+      // For each muliplexer
+     for (unsigned i = 0; i < 3; i++)
+     {
+        // Set the select of the output mux to the desired intermediary output
+        digitalWrite(output_sel[0], (line*3 + i) & 0x1);
+        digitalWrite(output_sel[1], (line*3 + i) & 0x2);
+        digitalWrite(output_sel[2], (line*3 + i) & 0x4);
+                
+        // For each input to the intermediary multiplexer
+        for (unsigned short j = 0; j < 8 && (i*8 + j) <= 18; j++)
+        { 
+          // Set select to that input
+          digitalWrite(intermediary_sel[0], j & 0x1);
+          digitalWrite(intermediary_sel[1], j & 0x2);
+          digitalWrite(intermediary_sel[2], j & 0x4);
+
+          // Room for 24 reads per intermediary set but only using 18 of them
+          voltages[line][(i*8 + j)] = analogRead(output) * (input_voltage / 1023.0);
+          Serial.print(voltages[line][(i*8 + j)]);
+          Serial.print("\t");
+        }  
       }
-
       
-   }
+      Serial.print ("\n");      
+    }
+
+        
+      
+
+   
 
 /*
     for (unsigned v = 0; v < 20; v++)
@@ -64,7 +69,7 @@ void loop()
       Serial.print ("\t");
     }
 */   
-   Serial.print ("\n\n");
+   Serial.print ("\n");
   
  
     
