699d771435b7bee981f7e1000f0236c89229650a
Your Name <you@example.com>
2017-06-19 15:42:20 +0000

    reads rods

--- hub_comms/bt.py
+++ hub_comms/bt.py
@@ -1,37 +1,70 @@
-# file: l2capclient.py
-# desc: Demo L2CAP client for bluetooth module.
-# $Id: l2capclient.py 524 2007-08-15 04:04:52Z albert $
+demo = True
 
 import sys
 import bluetooth
 import subprocess
+import select
+from copy import deepcopy
+
+def recv(sock, length):
+    data = ''
+    while len(data) < length:
+        packet = sock.recv(length - len(data))
+        if not packet:
+            return None
+        data += packet
+    return data
 
 if sys.version < '3':
     input = raw_input
 
-
-
 if len(sys.argv) < 2:
-    print("usage: bt.py <addr>")
+    print("usage: bt.py <address_filename>")
     sys.exit(2)
 
+address_filename = sys.argv[1]
 
-bt_addr=sys.argv[1]
+address_file = open(address_filename, 'r')
+addresses = address_file.readlines()
+address_file.close()
+
 port = 0x1001
+addr_socket={}
+board_id = 0
+addr_id = {}
 
-print("trying to connect to %s on PSM 0x%X" % (bt_addr, port))
-
-sock=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-sock.connect((bt_addr, port))
-
-print("connected.  type stuff")
+for addr in addresses:
+	a = addr.rstrip('\n\r')
+	print("Trying to connect to %s on port 0x%X" % (a, port))
+	addr_socket[a]=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+	addr_socket[a].connect((a, port))
+	addr_id[a] = board_id
+	print("Connected.")
+		
+	port += 1
+	board_id += 1
+		
+socket_addr = dict((reversed(item) for item in addr_socket.items()))
+socket_list = addr_socket.values()
 while True:
-    data = input()
-    if(len(data) == 0): break
-    sock.send(data)
-    data = sock.recv(1024)
-    print("Data received:", str(data))
+	read_sockets, write_sockets, error_sockets = select.select(socket_list , [] , [])
 
+	for s in read_sockets:
+			# Get message length
+			raw_length = recv(s, 4)
+			length = int(raw_length)
+			
+			# Read that many chars
+			data = recv(s, length)
+			if data[0] == '0':
+				rods = data[1:]
+				s.send("ACK:" + rods + '\n\r')
+			
+				if demo:
+					rods = rods[0:-1]
+					rods += ',[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]]'
+
+					
 sock.close()
 
 
